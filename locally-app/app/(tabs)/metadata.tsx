25a712d2ab94daeb8c59723f9cd0a10f74d744c4
Jaime <nguyenphuocdat168@gmail.com>
2024-11-02 20:34:12 -0400

    header

--- locally-app/app/(tabs)/metadata.tsx
+++ locally-app/app/(tabs)/metadata.tsx
@@ -1,14 +1,46 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, SafeAreaView } from 'react-native'
 import React from 'react'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 const Metadata = () => {
   return (
-    <View className="flex-1 justify-center items-center bg-white">
-      <View className="bg-[#003566] p-4 rounded-md">
-        <Text className="text-white text-lg font-bold">Jaime</Text>
-      </View>
-    </View>
+    <SafeAreaView className='h-full'>
+      <ScrollView className="p-4 space-y-6">
+        <Header/>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
 export default Metadata
+
+// Header component
+const Header = () => {
+  return (
+    <View className="justify-between items-start flex-row mb-6">
+      <View>
+        <Text className="text-md text-gray-400">
+          Go Exploring,
+        </Text>
+        <Text className="text-2xl font-semibold text-primary-pBlue">
+          Jaime
+        </Text>
+      </View>
+
+      <View className="flex-row items-center">
+        <View className='items-end px-2'>
+          <Text className="text-md font-medium text-gray-900">
+            Current location
+          </Text>
+          <Text className="text-lg font-medium text-primary-pBlue">
+            New York, US
+          </Text>
+        </View>
+
+        <View className="w-14 h-14 bg-gray-200 rounded-full items-center justify-center">
+          <MaterialCommunityIcons name="map-marker-outline" size={30} color="#003566" />
+        </View>
+      </View>
+    </View>   
+  )
+}
