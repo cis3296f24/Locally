0de1f4518856c25a3d98e45762a7616e5b28a03a
Jaime <nguyenphuocdat168@gmail.com>
2024-11-04 00:04:43 -0500

    restructure

--- locally-app/app/index.tsx
+++ locally-app/app/index.tsx
@@ -1,16 +1,18 @@
-import { Link } from "expo-router";
+import { Link, Redirect } from "expo-router";
 import { Text, View } from "react-native";
-// import {TabsLayout} from "./TabsLayout";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center bg-white">
-      <Link
-        href="/(tabs)/metadata"
-        className="text-blue-600 font-bold text-2xl"
-      >
-        <Text>Go to Tab View</Text>
-      </Link>
-    </View>
+    <Redirect href='/(tabs)/explore' />
+
   );
 }
+
+    // <View className="flex-1 justify-center items-center bg-white">
+    //   <Link
+    //     href="/(tabs)/metadata"
+    //     className="text-blue-600 font-bold text-2xl"
+    //   >
+    //     <Text>Go to Tab View</Text>
+    //   </Link>
+    // </View>
